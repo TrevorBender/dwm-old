0ab1a26547096577dc6bc4fb07a853da6e6807fb
TrevorBender <Trevor.Bender@gmail.com>
2011-09-23 00:20:21 -0400

    add options to test script

--- test.sh
+++ test.sh
@@ -5,5 +5,14 @@ export MY_DISPLAY=:2
 Xephyr -ac -br -noreset -screen 800x600 $MY_DISPLAY &
 sleep 1
 ulimit -c unlimited
+if [[ $# -gt 0 ]] ;  then
+    if [[ $1 = "-d" ]] ; then
+        DISPLAY=$MY_DISPLAY gdb ./dwm
+        exit 0
+    elif [[ $1 = "-l" ]] ; then
+        DISPLAY=$MY_DISPLAY ./dwm
+        exit 0
+    fi
+fi
 DISPLAY=$MY_DISPLAY ./scripts/dwm-start.sh
 echo "return = $?"
