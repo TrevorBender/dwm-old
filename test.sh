12000a82e485dfd817d51238e8d7bc2f3cda364b
TrevorBender <Trevor.Bender@gmail.com>
2011-10-09 16:07:08 -0400

    update test script - include valgrind

--- test.sh
+++ test.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
 export MY_DISPLAY=:2
+usage() {
+    echo "Usage: $0 [-d|s|v]"
+    echo " -d Run in gdb"
+    echo " -s Run using dwm-start script"
+    echo " -v Run in valgrind"
+}
 
+command=./dwm
+while getopts dsv opt ; do
+    case "$opt" in
+        d) command="gdb ./dwm"
+            ;;
+        s) command=./scripts/dwm-start.sh 
+            ;;
+        v) command="valgrind ./dwm"
+            ;;
+        \?)
+            #echo "Invalid option: -$OPTARG"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+#Start windowed X11 instance
 Xephyr -ac -br -noreset -screen 800x600 $MY_DISPLAY &
 sleep 1
 ulimit -c unlimited
-if [[ $# -gt 0 ]] ;  then
-    if [[ $1 = "-d" ]] ; then
-        DISPLAY=$MY_DISPLAY gdb ./dwm
-        exit 0
-    elif [[ $1 = "-l" ]] ; then
-DISPLAY=$MY_DISPLAY ./scripts/dwm-start.sh
-        exit 0
-    fi
-fi
-DISPLAY=$MY_DISPLAY ./dwm
+
+# and run dwm
+DISPLAY=$MY_DISPLAY $command
 echo "return = $?"
