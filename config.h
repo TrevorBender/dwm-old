f5d91cc4b38a0540cd146f8b806b101a25c5e83e
TrevorBender <Trevor.Bender@gmail.com>
2012-04-25 09:09:42 -0400

    remove fibonaci and move  book to separate file

--- config.h
+++ config.h
@@ -28,14 +28,12 @@ static const float mfact      = 0.55; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
-#include "fibonacci.c"
+#include "book.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },    /* first entry is default */
     { "><>",      NULL },    /* no layout function means floating behavior */
     { "[M]",      monocle },
-    { "[@]",      spiral },
-    { "[//]",     dwindle },
     { "[I]",      book },
 };
 
