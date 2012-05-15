debf2710d8beb7a3d8e657bfdad1622f9d657ce5
TrevorBender <Trevor.Bender@xerox.com>
2012-05-15 10:08:09 -0400

    default master size is .5

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
